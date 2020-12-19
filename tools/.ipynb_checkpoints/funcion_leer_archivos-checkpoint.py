924e5575922fec3a1a9f572acf2524e1a0f1a79b
esgalu <58479403+esgalu@users.noreply.github.com>
2020-12-19 17:16:01 -0600

    feat: (@esgalu) se crean funcion de lectura de archivos

--- tools/.ipynb_checkpoints/funcion_leer_archivos-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def leer_archivos(path):
-    try:
-        df = pd.read_csv(path, encoding='unicode-escape')
-    except:
-        df = pd.read_csv(path, encoding='unicode-escape', sep=';')
-    return df
