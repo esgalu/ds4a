d6b97d330760ed221f0dbafb66fef5cb938e7bcd
esgalu <58479403+esgalu@users.noreply.github.com>
2020-12-19 17:11:52 -0600

    feat: (@esgalu) se crea docstring

--- tools/funcion_leer_archivos.py
+++ tools/funcion_leer_archivos.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
 
-def leer_archivos(path):
+def leer_archivos(path:str):
+    """
+    function in charge of reading files in a certain path with .csv format,
+     try to do it with a conventional separator or separated by semicolons.
+    Args:
+        path: (str) path of file
+    Returns:
+        pd.Dataframe: dataframe
+    """
     try:
         df = pd.read_csv(path, encoding='unicode-escape')
     except:
